be42627844a1f754bca17cb65a6186384d98ca84
fakhryhizballah <falehry88@gmail.com>
2026-06-10 22:50:12 +0700

    dokumen v1

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const controller = require('../controllers');
 const verifyToken = require('../middlewares/authMiddleware');
 
 // Routes
-router.get('/v1', verifyToken, controller.getAllDocuments);
-router.post('/v1', verifyToken, controller.postDocument);
-router.delete('/v1/:id', verifyToken, controller.deleteDocument);
-router.put('/v1/:id', verifyToken, controller.updateDocument);
+router.get('/v1/dokumen', verifyToken, controller.getAllDocuments);
+router.post('/v1/dokumen', verifyToken, controller.postDocument);
+router.delete('/v1/dokumen/:id', verifyToken, controller.deleteDocument);
+router.put('/v1/dokumen/:id', verifyToken, controller.updateDocument);
 
 module.exports = router;
